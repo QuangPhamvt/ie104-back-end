92fe72fab50958760160cec0289fbd44d8d8b2ee
QuangPM - VFA <quangpm220503vt@gmail.com>
2023-11-14 13:52:04 +0700

    refactor: auth api

--- src/modules/auth.module/auth.model.ts
+++ src/modules/auth.module/auth.model.ts
@@ -8,12 +8,16 @@ export const signInDto = t.Object({
   password: t.String({ description: "This is password", default: "123456" }),
 })
 
-export const signUpDto = t.Object({
-  email: t.String({ format: "email", default: "buyer@example.com" }),
-  password: t.String({ description: "This is password", default: "123456" }),
-  username: t.String({ default: "buyer" }),
-  role: t.Enum(ROLE, { default: "buyer" }),
-})
+export const signUpDto = t.Partial(
+  t.Object({
+    email: t.String({ format: "email", default: "buyer@example.com" }),
+    password: t.String({ description: "This is password", default: "123456" }),
+    username: t.String({ default: "buyer" }),
+    role: t.Enum(ROLE, { default: "buyer" }),
+    arqId: t.String(),
+    accountNo: t.String(),
+  }),
+)
 export const refreshTokenDto = t.Object({
   refresh_token: t.String(),
 })
