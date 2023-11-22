12830ea3d1528a55782c1a1f2f5e936d67d3f98a
QuangPM - VFA <quangpm220503vt@gmail.com>
2023-11-22 10:23:37 +0700

    feat: add order

--- src/modules/order.module/service/updateStatusOrder.ts
+++ src/modules/order.module/service/updateStatusOrder.ts
@@ -14,18 +14,28 @@ export const updateStatusOrder = async <T extends updateStatusOrderDto>(props: T
   const { headers, body, set } = props
   const seller_id = headers.get("userId")
   const role = headers.get("role")
+  if (role !== "seller") {
+    set.status = 400
+    return {
+      message: "Bad request",
+    }
+  }
   try {
+    const [order] = await db.select().from(orders).where(like(orders.id, body.order_id))
     if (body.status === "deny") {
       await db.update(orders).set({ status: "deny" }).where(like(orders.id, body.order_id))
+      await db.update(carts).set({ status: "deny" }).where(like(carts.id, order.cart_id))
     }
     if (body.status === "ordered") {
       await db.update(orders).set({ status: "ordered" }).where(like(orders.id, body.order_id))
+      await db.update(carts).set({ status: "ordered" }).where(like(carts.id, order.cart_id))
     }
     return { message: "Oke" }
   } catch (error) {
     console.log(error)
+    set.status = "Internal Server Error"
     return {
-      message: "Oke",
+      message: "Internal Server Error",
     }
   }
 }
